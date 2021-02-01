86827ea3549add8ce3cd28b90e734e2e13d97618
Metalcoder <yunior.rdguez@gmail.com>
2021-02-01 19:52:29 +0000

    Fix html, css and some of the javascript

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + years;
+}
+
+function showRange()
+{
+    var rangeSpan = document.getElementById("rate-span");
+    rangeSpan.innerHTML = document.getElementById("rate").value + ' %';
 }
         
