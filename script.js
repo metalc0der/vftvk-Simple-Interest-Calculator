a8c34bb0536ecf1f98149002fa335ecda0d3ad42
Yunior Rodriguez <yunior.rdguez@gmail.com>
2021-02-02 01:55:11 +0000

    Last changes

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+
+    // Validating that principal is a positive number
+    if (principal <= 0) {
+        alert('Enter a positive number');
+        document.getElementById("principal").focus();
+
+        // Do not compute if principal is 0 or less than 0
+        return;
+    }
+
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var years = Number(document.getElementById("years").value);
     var interest = principal * years * rate / 100;
-    var year = new Date().getFullYear() + years;
+    var year = Number(new Date().getFullYear()) + years;
+    var result = document.getElementById("result");
+
+    // Forming innerHTML that will go into result
+    var html = '<br>';
+    html += 'If you deposit <mark>' + principal + '</mark>,<br>';
+    html += 'at an interest rate of <mark>' + rate + '%</mark>.<br>';
+    html += 'You will receive an amount of <mark>' + interest + '</mark>,<br>';
+    html += 'in the year <mark>' + year + '</mark>';
+    
+    result.innerHTML = html;
 }
 
 function showRange()
